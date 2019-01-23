b10aa708b76e06d2173e46a74d95b65944e0da1b
Eduardo Navarro Poveda <edunavarro13@gmail.com>
2019-01-23 18:00:51 +0100

    Empezando log out

--- src/app/page-view/page-view.component.ts
+++ src/app/page-view/page-view.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-page-view',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PageViewComponent implements OnInit {
 
-  constructor() { }
+  constructor(private routerLog: Router) { }
 
   ngOnInit() {
   }
 
+  logOut() {
+    if (confirm(`Are you sure you want to Log out?`)) {
+      localStorage.clear();
+      this.routerLog.navigate(['/login']);
+    }
+  }
 }
