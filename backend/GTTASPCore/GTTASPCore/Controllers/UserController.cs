72134d239dc2d6a385b0f994820e0c8b003221b9
Eduardo Navarro Poveda <edunavarro13@gmail.com>
2019-01-24 19:00:34 +0100

    Agregado backend de Usuarios

---
+++ backend/GTTASPCore/GTTASPCore/Controllers/UserController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using GTTASPCore.Models;
+
+namespace GTTASPCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserContext _context;
+
+        public UserController(UserContext context)
+        {
+            this._context = context;
+            if(this._context.Users.Count() == 0)
+            {
+                Console.WriteLine("No existe ningun usuario");
+                User newUser = new User();
+                newUser.username = "edunavarro13";
+                newUser.password = "1234";
+                this._context.Users.Add(newUser);
+                this._context.SaveChanges();
+            }
+        }
+
+        // GET: api/User
+        [HttpGet]
+        public ActionResult<List<User>> GetAll()
+        {
+            return this._context.Users.ToList();
+        }
+
+        // GET: api/User/5
+        [HttpGet("{id}", Name = "Get")]
+        public ActionResult<User> Get(long id)
+        {
+            User userGet = this._context.Users.Find(id);
+            if(userGet == null)
+            {
+                return NotFound("Error 404");
+            }
+            return userGet;
+        }
+
+        // POST: api/User
+        [HttpPost]
+        public ActionResult<User> Post([FromBody] User value)
+        {
+            this._context.Users.Add(value);
+            this._context.SaveChanges();
+            return value;
+        }
+
+        // PUT: api/User/5
+        [HttpPut("{id}")]
+        public void Put(long id, [FromBody] User value)
+        {
+            User userUpdate = this._context.Users.Find(id);
+            userUpdate.username = value.username;
+            userUpdate.password = value.password;
+            this._context.SaveChanges();
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public ActionResult<string> Delete(long id)
+        {
+            // Se podria tambien hacer con Find
+            try
+            {
+                User userDel = this._context.Users.Where(user => user.id == id).First();
+                this._context.Remove(userDel);
+                this._context.SaveChanges();
+                return "200";
+            } catch(Exception ex)
+            {
+                return NotFound("Error 404");
+            }
+        }
+    }
+}
